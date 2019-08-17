064f803eb34c55474f60ae95376d50f8c5a7dc7a
Yeongbin Jo <iam.yeongbin.jo@gmail.com>
2019-08-17 18:43:14 +0900

    0.0.2

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 # coding: utf-8
+import os
+import sys
 
 from setuptools import setup
-from setuptools.command.build_py import build_py
-from chromedriver_autoinstaller.utils import download_chromedriver
 
 
 __author__ = 'Yeongbin Jo <iam.yeongbin.jo@gmail.com>'
@@ -12,29 +12,23 @@ with open('README.md') as readme_file:
     long_description = readme_file.read()
 
 
-class DownloadChromedriver(build_py):
-    def run(self):
-        """
-        Downloads, unzips and installs chromedriver.
-        If a chromedriver binary is found in PATH it will be copied, otherwise downloaded.
-        """
-        download_chromedriver()
-        build_py.run(self)
+# 'setup.py publish' shortcut.
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist bdist_wheel')
+    os.system('twine upload dist/*')
+    sys.exit()
 
 
 setup(
     name="chromedriver-autoinstaller",
-    version="0.0.1",
+    version="0.0.2",
     author="Yeongbin Jo",
     author_email="iam.yeongbin.jo@gmail.com",
-    description="Auto installer for chromedriver.",
+    description="Automatically install chromedriver that supports the currently installed version of chrome.",
     license="MIT",
     keywords="chromedriver chrome selenium splinter",
     url="https://github.com/yeongbin-jo/python-chromedriver-autoinstaller",
     packages=['chromedriver_autoinstaller'],
-    package_data={
-        'chromedriver_autoinstaller': ['chromedriver*']
-    },
     entry_points={
         'console_scripts': ['chromedriver-path=chromedriver_autoinstaller.utils:print_chromedriver_path'],
     },
@@ -55,5 +49,4 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
-    cmdclass={'build_py': DownloadChromedriver}
 )
