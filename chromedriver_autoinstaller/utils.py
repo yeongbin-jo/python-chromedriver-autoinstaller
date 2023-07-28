ba3e8aca24c70ebe198db655988d147bbabc2ef3
Elias.yr.Li <45684547+OrekiYuta@users.noreply.github.com>
2023-07-28 22:31:33 +0800

    fix: fix dir path not found error in win
    if win path not exist will throw error
    
    such as below logging:
    ```
      File "..\venv\lib\site-packages\chromedriver_autoinstaller\__init__.py", line 21, in install
        chromedriver_filepath = utils.download_chromedriver(path, no_ssl)
      File "..\venv\lib\site-packages\chromedriver_autoinstaller\utils.py", line 266, in download_chromedriver
        chrome_version = get_chrome_version()
      File "..\venv\lib\site-packages\chromedriver_autoinstaller\utils.py", line 170, in get_chrome_version
        dirs = [f.name for f in os.scandir("C:\\Program Files\\Google\\Chrome\\Application") if f.is_dir() and re.match("^[0-9.]+$", f.name)]
    FileNotFoundError: [WinError 3] The system cannot find the path specified: 'C:\\Program Files\\Google\\Chrome\\Application'
    ```

--- chromedriver_autoinstaller/utils.py
+++ chromedriver_autoinstaller/utils.py
@@ -167,12 +167,16 @@ def get_chrome_version():
     elif platform == "win":
         # check both of Program Files and Program Files (x86).
         # if the version isn't found on both of them, version is an empty string.
-        dirs = [f.name for f in os.scandir("C:\\Program Files\\Google\\Chrome\\Application") if f.is_dir() and re.match("^[0-9.]+$", f.name)]
-        if dirs:
-            version = max(dirs)
-        else:
-            dirs = [f.name for f in os.scandir("C:\\Program Files (x86)\\Google\\Chrome\\Application") if f.is_dir() and re.match("^[0-9.]+$", f.name)]
+
+        if not os.path.exists(chrome_path):
+            chrome_path = "C:\\Program Files (x86)\\Google\\Chrome\\Application"
+
+        try:
+            dirs = [f.name for f in os.scandir(chrome_path) if f.is_dir() and re.match("^[0-9.]+$", f.name)]
             version = max(dirs) if dirs else ''
+        except FileNotFoundError:
+            version = ''
+            
     else:
         return
     return version
