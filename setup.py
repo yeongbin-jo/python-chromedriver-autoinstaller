9a929d330d99ce92066658a8b8a9784bdbebb0b4
shawnCaza <theshawn@gmail.com>
2023-08-07 00:22:07 -0400

    Adding 'packaging' lib requirement to setup

--- setup.py
+++ setup.py
@@ -37,6 +37,9 @@ setup(
     keywords="chromedriver chrome chromium selenium",
     url="https://github.com/yeongbin-jo/python-chromedriver-autoinstaller",
     packages=["chromedriver_autoinstaller"],
+    install_requires=[
+          'packaging',
+      ],
     entry_points={
         "console_scripts": [
             "chromedriver-path=chromedriver_autoinstaller.utils:print_chromedriver_path"
@@ -57,5 +60,6 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
 )
